5f9f035e3f700af8d26896993a37dc5c682ce5d8
danapmontalvo200 <danapaolamontalvocuna@gmail.com>
2019-10-28 21:42:44 -0600

    commit

---
+++ 04_introJavaSpript/main.js
@@ -0,0 +1,105 @@
+//Comprobar si esta bien linkeada
+//prompt( 'holaaa')
+//var numero=10
+
+//let numero = 10;
+//valor spring o texto
+//let texto =  "Dana";
+//let terxto2 = 'Dana2';
+
+//let verdadero = true;
+//let falso = false;
+
+//objeto
+//let object = {}
+//arreglo
+//let array = []
+
+
+//_____-
+//let x = 100;
+//let y = 50;
+//let suma = x + y
+//console.log( x + y)
+//console.log(suma)
+
+// + - * / %
+
+//let cambiar = 1500;
+//console.log (cambiar);
+//cambiar = 10
+//console.log(cambiar);
+//cambiar = "texto";
+//console.log(cambiar);
+
+//Arrays
+//let frutas = ["melon", "sandia", "fresa"];
+//console.log(frutas[1])
+//frutas[3] = "manzana";
+//frutas.push("guayaba");
+//frutas.pop();
+//frutas.pop()
+//frutas.splice(1, 2)
+//console.log(frutas);
+
+
+//object
+
+//let persona = {
+    ///nombre : "Dana",
+    //direccion : "Coacalco",
+    //telefono : "5532568709",
+    //email : "danapaolamonyalvocuna@gmail.com",
+    //edad : "17",
+    //trabajo: ["dev.f", "develope", "mami"],
+    //mascotas : {
+        //perros: "salchicha",
+        //gatos: "siames",
+        //pez: "dorado",
+   // }
+//}
+//console.log(persona.mascotas.pez);
+
+
+//let nombre =prompt("nombre");
+
+//let edad =prompt("edad");
+
+//let telefono=prompt("telefono");
+
+//NaN-> not a number
+//console.log("nombre" + nombre);
+//console.log("edad" + edad);
+//console.log("telefono" + telefono);
+
+//let fecha =prompt("fecha de nacimiento");
+//let anioactual = 2019;
+
+//let anodenacimiento=Number(anioactual -fecha) ;
+//console.log(anioactual - fecha)
+
+
+//object
+let persona = {
+    nomnbre:"dana",
+    direccion: "jfdfdfnd",
+    telefono: "kffjefjer",
+    peliculas: {
+        accion: "avengers",
+        terror: "conjuro",
+        comedia: "son como niños",
+        romance: "el diario de una pasion",
+        drama:"y si me quedo",
+    },
+
+    musica: {
+        justinBieber: "baby",
+        paolaMami:"fingias",
+        tangana:"no te debi besar",
+        rosalia:"con altura",
+        relsB:"quedate, te quiero"
+    }
+comida: {
+    tacos
+}
+}
