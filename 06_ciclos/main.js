dda241e72fcdfdcc3751fb6aa170651d76dc2411
danapmontalvo200 <danapaolamontalvocuna@gmail.com>
2019-10-30 21:49:31 -0600

    tercero commit

---
+++ 06_ciclos/main.js
@@ -0,0 +1,41 @@
+// for(let i = 0;i <= 100; i = i + 1){
+// console.log(i);
+// }
+
+
+//ejercicio 1
+//empiece en 10 termine en 1000 de 5 en 5
+
+// for(let i = 10; i <=1000; i = i + 5){
+//     console.log(i);
+// }
+
+//ejercicio2
+//empiece en 500 termine en 0 de 2 en 2
+
+// for (let i = 500; i >= 0; i = i - 2){
+//     console.log(i)
+// }
+
+//ejercicio 3
+//empiece en -763 termine en 15 de 3 en 3
+
+// for (let i = -762; i <= 15; i = i + 3){
+//     console.log(i)
+// }
+
+//ejercicio 4
+
+// for (let i = 1; i <= 100; i++) {
+// if (i % 3 === 0 && i % 5 === 0){
+// console.log(i + "fizzBuzz")
+// }else 
+// if(i % 5 === 0){
+//     console.log (i + "buzz")
+// } else if (i % 3 === 0){
+//     console.log (i + "fizz")
+// }else{
+//     console.log(i);
+// }
+// }
+
